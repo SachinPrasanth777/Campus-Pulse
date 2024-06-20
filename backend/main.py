4d1f490d5c9f85bbfd1f8b3edbe857cc7140ed33
SachinPrasanth777 <142246653+SachinPrasanth777@users.noreply.github.com>
2024-06-20 12:19:23 +0530

    fix: server crash

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,11 @@ from starlette.middleware.sessions import SessionMiddleware
 from fastapi.middleware.cors import CORSMiddleware
 from loginauth.auth import auth_router
 from loginauth.routes import protected_router
-from adminpage.routes.admin import router
+from adminpage.utilities.database import Database
+from adminpage.routes import admin
 
 app = FastAPI()
-
+db=Database()
 SECRET_KEY = "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"
 app.add_middleware(SessionMiddleware, secret_key=SECRET_KEY)
 app.add_middleware(
@@ -19,8 +20,7 @@ app.add_middleware(
 
 app.include_router(auth_router)
 app.include_router(protected_router)
-
-app.include_router(router)
+app.include_router(admin.router)
 
 
 @app.get('/')
